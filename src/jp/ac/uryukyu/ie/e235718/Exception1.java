88c2c195d781788f1b341ae502fd3c252db6af9c
Ibuki Shinzato <e235718@ie.u-ryukyu.ac.jp>
2023-10-27 15:02:57 +0900

    finish try_catch

---
+++ src/jp/ac/uryukyu/ie/e235718/Exception1.java
@@ -0,0 +1,15 @@
+package jp.ac.uryukyu.ie.e235718;
+
+import java.io.FileWriter;
+
+public class Exception1 {
+    public static void main(String[] args){
+	// try-catchなしのコード。
+	FileWriter fw = new FileWriter("output.txt");
+	String[] results = {"ほげ", "ふが"};
+	for(String temp: results){
+	    fw.write(temp);
+	}
+	fw.close();
+    }
+}
---
+++ src/jp/ac/uryukyu/ie/e235718/Exception2.java
@@ -0,0 +1,20 @@
+package jp.ac.uryukyu.ie.e235718;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Exception2 {
+    public static void main(String[] args){
+	// try-catchありのコード。
+        try{
+            FileWriter fw = new FileWriter("output.txt");
+            String[] results = {"ほげ", "ふが"};
+            for(String temp: results){
+                fw.write(temp);
+            }
+	        fw.close();
+	    }catch (IOException e) {
+	        System.out.println(e.getMessage());
+	    }
+    }
+}
